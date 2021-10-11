083196e64cfebdf891ed1957ec02d7df2bd4bb1f
ahmedmawardy-chd <ahmedelmawrdy2@gmail.com>
2021-10-11 18:14:02 +0200

    port to 80

--- small_library/settings.py
+++ small_library/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s8687%wp5=o4@v0-*_=lrb=83u!1btb!*1&4n1=nl*vp6dbe8g'
+SECRET_KEY = 's8687%wp5=o4@v0-*_=lrb=83u!1btb!*1&4n1=nl*vp6dbe8g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
